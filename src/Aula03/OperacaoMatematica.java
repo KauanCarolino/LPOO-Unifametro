3a322959df189705a748235fec11956489def9ae
Kauan Carolino <kauancarolino248@gmail.com>
2023-04-27 18:48:33 -0300

    Interfaces

---
+++ src/Aula03/OperacaoMatematica.java
@@ -0,0 +1,8 @@
+package Aula03;
+
+public interface OperacaoMatematica {
+    void soma(double operando1, double Operando2);
+    void subtracao(double operando1, double Operando2);
+    void multiplicacao(double operando1, double Operando2);
+    void divisao(double operando1, double Operando2);
+}
